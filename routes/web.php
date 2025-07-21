2665cc36af31ebfa41d2d4987df8de19fe4644b2
Rafli Ilham Aditia <ilhamrafli732@gmail.com>
2025-07-21 19:58:16 +0700

    fixing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\sendEmailsController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/send', [sendEmailsController::class, 'send']);
